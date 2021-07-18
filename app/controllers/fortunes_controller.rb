b5b9630f59be2c06110d4e2ca1c55e96143f6b15
gb-appdev <sbhaktha@chicagobooth.edu>
2021-07-18 01:19:29 +0000

    finished exercise

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,18 @@
+class FortunesController < ApplicationController
+
+  def horoscopes
+
+    @sign = params.fetch("the_sign")
+
+
+    all_zodiacs = Zodiac.list
+    this_zodiac = all_zodiacs.fetch(@sign.to_sym)
+    @horoscope = this_zodiac.fetch(:horoscope)
+
+    @array_of_numbers = Zodiac.lucky_numbers
+
+    render({ :template => "fortune_templates/horo.html.erb" })
+
+  end
+
+  end
